5a4e172b683841ec3afe1771e7c87decf465f0a9
Hadirizaldi <Hadirizaldi.hr@gmail.com>
2021-04-17 21:55:32 +0700

    menambahkan properti asset pada shared

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bwa_flutix/services/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-import 'bloc/page_bloc.dart';
+import 'bloc/bloc.dart';
 import 'ui/pages/pages.dart';
 
 void main () {
@@ -17,7 +17,8 @@ class  MyApp extends StatelessWidget {
       initialData: null,
       child: MultiBlocProvider(
         providers: [
-          BlocProvider(create: (_) => PageBloc())
+          BlocProvider(create: (_) => PageBloc()),
+          BlocProvider(create: (_) => UserBloc())
         ],
         child: MaterialApp(debugShowCheckedModeBanner: false, home: Wrapper())),
     ); 
