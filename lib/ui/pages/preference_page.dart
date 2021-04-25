d7b182833d15bf6ca947beb44554b792e2919e68
Hadirizaldi <Hadirizaldi.hr@gmail.com>
2021-04-25 11:28:36 +0700

    preference page mulai

--- lib/ui/pages/preference_page.dart
+++ lib/ui/pages/preference_page.dart
@@ -1,1 +1,15 @@
 part of 'pages.dart';
+
+class PreferencePages extends StatefulWidget {
+  @override
+  _PreferencePagesState createState() => _PreferencePagesState();
+}
+
+class _PreferencePagesState extends State<PreferencePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
